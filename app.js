b6790bf10cee2900af0a7f781fb5fead07e28c4b
GuershomYao <yaoguershom.gassihoun@septeo.com>
2026-03-15 13:31:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+let invites = {};
+
+fetch("invites.json")
+.then(response => response.json())
+.then(data => invites = data);
+
+function onScanSuccess(decodedText) {
+
+ let guest = invites[decodedText];
+
+ if(!guest){
+   document.getElementById("result").innerHTML =
+   "❌ Invité inconnu";
+   return;
+ }
+
+ if(guest.used){
+   document.getElementById("result").innerHTML =
+   "⚠️ Déjà utilisé : " + guest.nom;
+   return;
+ }
+
+ guest.used = true;
+
+ document.getElementById("result").innerHTML =
+   "✅ " + guest.nom +
+   "<br>Table : " + guest.table;
+}
+
+const scanner = new Html5QrcodeScanner(
+ "reader",
+ { fps: 10, qrbox: 250 }
+);
+
+scanner.render(onScanSuccess);
